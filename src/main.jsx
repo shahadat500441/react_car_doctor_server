179867657f02d5abd2112d7959ba664943298d54
shahadat500441 <smd643925@gmail.com>
2025-07-24 13:16:39 +0600

    banner section added

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,9 @@ import {
 import router from './Routes/Routes';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <div className=" max-w-7xl mx-auto">
+    <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
+  </div>
 )
